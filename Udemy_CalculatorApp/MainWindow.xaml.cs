f5f580862aac5c067296c3d0a9f6121b43993d10
Nicholas Kostas <nikobpi23@yahoo.com>
2022-03-19 10:58:06 -0400

    Starting defining event handlers for the buttons

--- Udemy_CalculatorApp/MainWindow.xaml.cs
+++ Udemy_CalculatorApp/MainWindow.xaml.cs
@@ -20,12 +20,89 @@ namespace Udemy_CalculatorApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Class variable definitions
+        /// </summary>
+        
+        double lastNumber, result;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            
 
-            //Testing accessing and modifying the label
-            resultLabel.Content = "123";
+            // Event Handlers Defitions
+            acButton.Click += AcButton_Click;
+            negativeButton.Click += NegativeButton_Click;
+            percentButton.Click += PercentButton_Click;
+            equalsButton.Click += EqualsButton_Click;
+        }
+
+        /// <summary>
+        /// Click event handler for the Equals Button on the Calculator Form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// Click Event Handler for the Percentage Button  on the calculator app
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PercentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber / 100;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Click Event Handler for the negative button on the calc app
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Click event handler for the AC button on the calculator
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+
+        /// <summary>
+        /// Click Event Handler for the Seven Button on the Calculator
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void sevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "7";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}7";
+            }
         }
     }
 }
